fc6a3bc51318d70ca3c155aa675eb46a04231014
PedroEM <ezemartinex@gmail.com>
2022-08-18 15:24:04 -0300

    Tp Nro4

---
+++ src/TpNro4/TpNro4.java
@@ -0,0 +1,10 @@
+package TpNro4;
+
+public class TpNro4 {
+    public static void main(String[] args) {
+        String nombre = "Ezequiel";
+        int edad = 22;
+        double salario = 100.24;
+        boolean carnet = true;
+    }
+}
