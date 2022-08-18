058237dfc0441ec8068da2ba6625af4b3e9e38ee
PedroEM <ezemartinex@gmail.com>
2022-08-18 15:36:51 -0300

    Tp Nro 5

---
+++ src/TpNro5/TpNro5.java
@@ -0,0 +1,12 @@
+package TpNro5;
+
+public class TpNro5 {
+    public static void main(String[] args) {
+        double numerador = (6.0 / 5.0) - (2.0 / 3.0) * (7.0 / 2.0) + (2.0 / 30.0);
+        double denominador = (1.0 / 3.0) / 5.0;
+        double resultado = numerador / denominador;
+        System.out.println("6/5 - 2/3 . 7/2 + 2/30");
+        System.out.println("----------------------   = " + resultado);
+        System.out.println("    1/3  % 5");
+    }
+}
